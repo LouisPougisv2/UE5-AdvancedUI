69a3cc6eb0da37fa6999f2bd62b865b8860c2aec
Louis <louis.pougis@gmail.com>
2025-11-06 12:15:09 +0100

    Set up (Last Part)
    - Press Any Key widget created and added to main screen
    - Using Developer settings to centralize Widget references
    - Advanced UI Function Library added to retrieve widget references from Developer settings

--- Source/UE5AdvancedUI/Private/AUIGameplayTags.cpp
+++ Source/UE5AdvancedUI/Private/AUIGameplayTags.cpp
@@ -4,8 +4,13 @@
 
 namespace FAdvancedUIGameplayTags
 {
+    // Advanced UI Widget Stack
     UE5ADVANCEDUI_API UE_DEFINE_GAMEPLAY_TAG(AdvancedUI_WidgetStack_Modal, "AdvancedUI.WidgetStack.Modal")
     UE5ADVANCEDUI_API UE_DEFINE_GAMEPLAY_TAG(AdvancedUI_WidgetStack_GameMenu, "AdvancedUI.WidgetStack.GameMenu")
     UE5ADVANCEDUI_API UE_DEFINE_GAMEPLAY_TAG(AdvancedUI_WidgetStack_GameHUD, "AdvancedUI.WidgetStack.GameHUD")
     UE5ADVANCEDUI_API UE_DEFINE_GAMEPLAY_TAG(AdvancedUI_WidgetStack_FrontEnd, "AdvancedUI.WidgetStack.FrontEnd")
+
+    // Advanced UI Widget
+    UE5ADVANCEDUI_API UE_DEFINE_GAMEPLAY_TAG(AdvancedUI_Widget_PressAnyKeyScreen, "AdvancedUI.Widget.PressAnyKeyScreen")
+    UE5ADVANCEDUI_API UE_DEFINE_GAMEPLAY_TAG(AdvancedUI_Widget_MainMenu, "AdvancedUI.Widget.MainMenu")
 }
--- Source/UE5AdvancedUI/Public/AUIGameplayTags.h
+++ Source/UE5AdvancedUI/Public/AUIGameplayTags.h
@@ -7,8 +7,13 @@
 
 namespace FAdvancedUIGameplayTags
 {
+    // Advanced UI Widget Stack
     UE5ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(AdvancedUI_WidgetStack_Modal)
     UE5ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(AdvancedUI_WidgetStack_GameMenu)
     UE5ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(AdvancedUI_WidgetStack_GameHUD)
     UE5ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(AdvancedUI_WidgetStack_FrontEnd)
+
+    // Advanced UI Widget
+    UE5ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(AdvancedUI_Widget_PressAnyKeyScreen)
+    UE5ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(AdvancedUI_Widget_MainMenu)
 }
